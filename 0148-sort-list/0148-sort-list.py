42dba8b3d86d23f238f0e78d6ddce3b32860864a
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-05-29 02:00:05 +0530

    Time: 342 ms (5.66%), Space: 33 MB (24.3%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -6,20 +6,58 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        ptr = head
-        ptr2 = head
-        a = []  
-        
-        while ptr!=None:
-            a.append(ptr.val)
-            ptr = ptr.next
-        print(a)
+        def middle(head):
+            temp = ListNode(0)
+            temp.next = head
 
-        a.sort()
-        while ptr2!=None:
-            ptr2.val = a[0]
-            a.pop(0)
-            
-            ptr2 = ptr2.next
+            ptr = temp
+            ptr2 = head
+            while ptr2 != None and ptr2.next != None:
+                ptr = ptr.next
+                ptr2 = ptr2.next.next
 
-        return head
+            return ptr
+
+        def merge(l1, l2):
+            ptr = l1
+            ptr2 = l2
+            l3 = ListNode(0)
+            ptr3 = l3
+
+            while ptr != None and ptr2 != None:
+                if ptr.val <= ptr2.val:
+                    ptr3.next = ptr
+                    temp = ptr
+                    ptr = ptr.next
+                    temp.next = None
+                    ptr3 = ptr3.next
+                else:
+                    ptr3.next = ptr2
+                    temp = ptr2
+                    ptr2 = ptr2.next
+                    temp.next = None
+                    ptr3 = ptr3.next
+
+            if ptr == None:
+                if ptr2 != None:
+                    ptr3.next = ptr2
+            elif ptr2 == None:
+                if ptr != None:
+                    ptr3.next = ptr
+
+            return l3.next
+
+        def divide(head):
+            if head == None or head.next == None:
+                return head
+
+            mid = middle(head)
+            temp = mid.next
+            mid.next = None
+
+            a1 = divide(head)
+            a2 = divide(temp)
+
+            return merge(a1, a2)
+
+        return divide(head)
