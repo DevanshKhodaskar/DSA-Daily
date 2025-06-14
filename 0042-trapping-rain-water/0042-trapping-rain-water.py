5806bc9f230d23f1d147f81f690a376d00cbc88c
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-06-14 20:49:25 +0530

    Time: 19 ms (42.33%), Space: 19.7 MB (5.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,30 +1,29 @@
-from typing import List
-
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
-        def calc(arr, height):
-            if len(arr) < 2:
-                return 0
-            threshold = min(height[arr[0]], height[arr[-1]])
-            ans = 0
-            for i in range(1, len(arr) - 1):
-                ans += max(0, threshold - height[arr[i]])
-            return ans
-
-        stack = []
-        ans = 0
+        left_max = []
+        right_max = []
+        l = height[0]
+        r = height[len(height)-1]
 
         for i in range(len(height)):
-            while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
-                if not stack:
-                    break  
-                distance = i - stack[-1] - 1
-                bounded_height = min(height[i], height[stack[-1]]) - height[top]
-                ans += distance * bounded_height
-            stack.append(i)
+            if height[i] > l:
+                l = height[i]
+            left_max.append(l)
+
+            if height[len(height)-1-i] > r:
+                r = height[len(height)-1-i]
+            right_max.append(r)
+        right_max.reverse()
+        ans = 0
+        for i in range(len(left_max)):
+            ans+= (min(left_max[i],right_max[i]) - height[i]) if (min(left_max[i],right_max[i]) - height[i]) > 0 else 0
 
         return ans
+
+        
+        
+
+
+
+
+        
