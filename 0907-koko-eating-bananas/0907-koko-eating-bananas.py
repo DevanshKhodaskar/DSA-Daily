1392e7fd7f1987aeada35b131dcd7e14262b351c
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-07-16 14:19:35 +0530

    Time: 314 ms (5.01%), Space: 18.7 MB (92.87%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -3,25 +3,26 @@ from typing import List
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        if len(piles) == 1:
-            return math.ceil(piles[0] / h)
-
-        minspeed = 1
-        maxspeed = max(piles)
-
-        while minspeed<maxspeed:
-            mid = (minspeed+maxspeed)//2
-            temp = 0
-            for i in piles:
-                temp +=math.ceil(i/mid)
-
-          
-            if temp>h:
-                minspeed=mid+1
-            else:
-                maxspeed=mid
-
-        return minspeed
-
         
+        def helper(piles,hr):
+            ans = 0
+            for i in piles:
+                ans+=math.ceil(i/hr)
+
+            return ans
+        maxx = max(piles)
+        l = 1
+        r = maxx
+
+        while l<r:
+            mid = (l+r)//2
+
+            temp = helper(piles,mid)
+            if helper(piles, mid) > h:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l
+
         
