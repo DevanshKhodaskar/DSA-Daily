09f8ba36a1f23ca413a9d00ea376068188de0eab
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-09-19 09:08:11 +0530

    Time: 8 ms (58.04%), Space: 18.6 MB (75.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,16 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = 0
+        l = 0 
         r = 0
         j = 0
-
-        while r< len(nums)-1:
-            temp = 0
+        steps = 0
+        while r<len(nums)-1:
+            temp  = 0
             for i in range(l,r+1):
-                temp = max(temp,i+nums[i])
-
+                temp = max(temp,nums[i]+i)
+            
+            steps += 1
             l = r+1
-            r= temp 
-            j +=1
+            r = temp
 
-        return j
+        return steps
+            
+        
