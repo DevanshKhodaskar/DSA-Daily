9dc042644ffff0abfc498c67e0de6cfc200a828c
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-12-03 14:07:21 +0530

    Time: 63 ms (45.12%), Space: 54 MB (45.33%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -11,39 +11,10 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         if not inorder or not postorder:
             return None
+        val = postorder.pop()
+        root = TreeNode(val)
+        mid  = inorder.index(val)
 
-    
-        postorder = postorder[::-1]
-
-        head = TreeNode(postorder[0])
-        self.postorderptr = 1
-        self.helper(head, postorder, inorder)
-        return head
-
-    def helper(self, head: TreeNode, postorder: List[int], arr: List[int]):
-        if not arr or self.postorderptr >= len(postorder):
-            return
-
-        for i in range(len(arr)):
-            if arr[i] == head.val:
-                id = i
-                break
-
-        l1 = arr[:id]     
-        l2 = arr[id + 1:]  
-
-       
-        if l2 and self.postorderptr < len(postorder):
-            v_right = postorder[self.postorderptr]
-            self.postorderptr += 1
-            rnode = TreeNode(v_right)
-            head.right = rnode
-            self.helper(rnode, postorder, l2)
-
-
-        if l1 and self.postorderptr < len(postorder):
-            v_left = postorder[self.postorderptr]
-            self.postorderptr += 1
-            lnode = TreeNode(v_left)
-            head.left = lnode
-            self.helper(lnode, postorder, l1)
+        root.right = self.buildTree(inorder[mid+1:],postorder)
+        root.left = self.buildTree(inorder[:mid],postorder)
+        return root
