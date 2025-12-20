d34f62a5c2c6a31e7c61971be215268dc5c5389e
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-12-20 12:23:26 +0530

    Time: 3 ms (76.57%), Space: 18.7 MB (99.01%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,35 +1,48 @@
-from typing import List
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         stack = []
-        dependency = [[] for _ in range(numCourses)]
+        ans = []
+        dependency = []
+        for _ in range(numCourses):
+            dependency.append([])
 
-        for a, b in prerequisites:
+        for a,b in prerequisites:
             dependency[b].append(a)
 
-        visited = [0] * numCourses  
-        cycle = False
+            
 
-        def dfs(cur):
+
+        cycle = False
+        visited = [0] * numCourses
+
+        def dfs(visited,stack,cur):
             nonlocal cycle
-            if visited[cur] == 1:
-                cycle = True
-                return
             if visited[cur] == 2:
                 return
+            if visited[cur] == 1:
+                cycle = True
+                return 
 
             visited[cur] = 1
             for i in dependency[cur]:
-                dfs(i)
+                dfs(visited,stack,i)
             visited[cur] = 2
             stack.append(cur)
+            
 
+        
         for i in range(numCourses):
             if visited[i] == 0:
-                dfs(i)
-
+                dfs(visited,stack,i)
         if cycle:
             return []
+        while stack:
+            ans.append(stack.pop())
 
-        return stack[::-1]
+        return ans
+                
+
+
+            
+
+        
