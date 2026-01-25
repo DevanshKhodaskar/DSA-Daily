9826923983f9c1f80b533595d5d915d85cd34701
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2026-01-26 01:11:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.49%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,24 +1,31 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        a = ""
-        s = s.lstrip()  
-        if len(s) == 0:
+        numbers = ['0','1','2','3','4','5','6','7','8','9']
+        if s == "" or s== "-":
             return 0
-
-        if s[0] == '-' or s[0] == '+':
-            a += s[0]
-            s = s[1:]
-
-        for i in s:
-            if i.isdigit():
-                a += i
+        b = s.split()
+        if not b:
+            return 0
+        ans = ""
+        for i in range(len(b[0])):
+            if i == 0 and b[0][i] == "-":
+                ans+="-"
+            elif i == 0 and b[0][i] == "+":
+                continue
             else:
-                break
-
-        if a == "" or a == "+" or a == "-":
+                if b[0][i] not in numbers:
+                    break
+                else:
+                    ans+=b[0][i]
+        if ans == "" or ans == "-":
             return 0
-
-        ans = int(a)
+        ans = int(ans)
         INT_MIN = -2**31
         INT_MAX = 2**31 - 1
-        return max(INT_MIN, min(INT_MAX, ans))
+
+        if ans < INT_MIN:
+            return INT_MIN
+        if ans > INT_MAX:
+            return INT_MAX
+
+        return ans
