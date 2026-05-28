187020fae8be2f52501468c099cf16cf2012c78c
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2026-05-28 21:57:00 +0530

    Time: 0 ms (100%), Space: 21 MB (75.42%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -3,34 +3,43 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ptr = ListNode()
-        ptr2 = ListNode()
 
-        p1 = ptr
-        p2 = ptr2
+        if head == None or head.next == None or head.next.next == None:
+            return head
 
-        temp = head
+        # count nodes
         count = 0
-        while temp != None:
-            if count%2 == 0:
-                p2.next = temp
-                p2 = p2.next
+        curr = head
+        while curr != None:
+            count += 1
+            curr = curr.next
 
-            else:
-                p1.next = temp
-                p1 = p1.next
-            count+=1
-            temp = temp.next
-        # if head.val%2 != 0:
-        #     new_head = ptr.next
-        #     p1.next = ptr2.next
-        #     p2.next = None
-        # else:
-        new_head = ptr2.next
-        p2.next = ptr.next
-        p1.next = None
-        return new_head
-        
-        
+        evenCount = count // 2
+
+        p1 = head
+        p2 = head
+
+        # original tail
+        while p1.next != None:
+            p1 = p1.next
+
+        # move even nodes
+        while evenCount > 0:
+
+            temp = p2.next
+
+            p2.next = temp.next
+
+            p1.next = temp
+            p1 = temp
+
+            temp.next = None
+
+            p2 = p2.next
+
+            evenCount -= 1
+
+        return head
