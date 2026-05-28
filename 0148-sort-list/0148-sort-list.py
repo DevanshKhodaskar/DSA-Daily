7b49b88d0ca569c9bb6b3cde61780444bdb33d78
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2026-05-28 23:23:35 +0530

    Time: 263 ms (8.09%), Space: 40.6 MB (60.54%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -1,56 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         if not head or not head.next:
             return head
+        
+
+        p1 = head
+        p2 = head.next 
+        p3 = head.next.next
 
         def breeak(head):
-            ptr3 = head
-            ptr = None
-            ptr2 = head
+            if not head or not head.next:
+                return head,None
+            p1 = head
+            p2 = head
+            p = ListNode(0)
+            p.next = head
+            while p1 and p1.next:
+                p2 = p2.next
+                p = p.next
+                p1 = p1.next.next
 
-            while ptr2 and ptr2.next:
-                ptr = ptr3
-                ptr3 = ptr3.next
-                ptr2 = ptr2.next.next
-
-            head2 = ptr3
-            if ptr:
-                ptr.next = None
-
-            return head, head2
-
-        def merge(head1, head2):
-            ptr = head1
-            ptr2 = head2
-
-            head = ListNode(0)
-            ans = head
-
-            while ptr and ptr2:
-                if ptr.val <= ptr2.val:
-                    head.next = ptr
-                    ptr = ptr.next
+            if p1 == None:
+                p.next = None
+                h1 = head
+                h2 = p2
+            else:
+                h2 = p2.next
+                h1 = head
+                p2.next = None
+            return h1,h2
+        
+        def merge(h1,h2):
+            x = ListNode(0)
+            temp = x
+            while h1 and h2:
+                if h1.val>h2.val:
+                    x.next = h2
+                    h2 = h2.next
+                    x = x.next
                 else:
-                    head.next = ptr2
-                    ptr2 = ptr2.next
-                head = head.next
+                    x.next = h1
+                    h1 = h1.next
+                    x = x.next
 
-            while ptr:
-                head.next = ptr
-                ptr = ptr.next
-                head = head.next
-
-            while ptr2:
-                head.next = ptr2
-                ptr2 = ptr2.next
-                head = head.next
-
-            return ans.next
-
-        head1, head2 = breeak(head)
-
-        left = self.sortList(head1)
-        right = self.sortList(head2)
-
-        return merge(left, right)
+            if h1:
+                x.next = h1
+                while x.next:
+                    x = x.next
+            elif h2:
+                x.next = h2
+                while x.next:
+                    x= x.next
+            return temp.next
+        h1,h2 = breeak(head)
+        h1,h2 = self.sortList(h1) ,self.sortList(h2)
+        return merge(h1,h2)
+        
+        
