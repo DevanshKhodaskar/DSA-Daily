0ff6340331786b4a8e877e72f1939a5cced0ac87
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2026-06-11 18:16:28 +0530

    Time: 17 ms (5.88%), Space: 23.4 MB (6.84%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,19 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-
         memo = {}
 
-        def helper(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-
-            if i == len(triangle) - 1:
-                return triangle[i][j]
-
-            left = helper(i + 1, j)
-            right = helper(i + 1, j + 1)
-
-            memo[(i, j)] = triangle[i][j] + min(left, right)
-            return memo[(i, j)]
-
-        return helper(0, 0)
+        for j in range(len(triangle[-1])):
+            memo[(len(triangle)-1),j] = triangle[(len(triangle)-1)][j]
+        def helper(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            else:
+                left = helper(i+1,j) if i<len(triangle)-1 else float("inf")
+                right = helper(i+1,j+1) if  i<len(triangle)-1 and j<len(triangle[i]) else float("inf")
+                
+                memo[(i,j)] = min(left,right)+triangle[i][j]
+                return memo[(i,j)]
+        return helper(0,0)
