ac80f1bbbb25537ed87396c22a60a84c9e31e23b
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2026-06-16 21:08:41 +0530

    Time: 12 ms (72.87%), Space: 19.6 MB (32.78%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,35 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        ans = []
+        visited = set()
+        ans.append(nums[0])
+        visited.add(nums[0])
+
+        def lower(ele):
+            l = 0
+            r = len(ans)-1
+            while l<=r:
+                m = (l+r)//2
+                if ans[m] >ele:
+                    r = m-1
+                else:
+                    l = m+1
+            return l
+
+
+
+        for i in range(1,len(nums)):
+
+            if nums[i]>ans[-1]:
+                ans.append(nums[i])
+            
+            elif nums[i]<ans[-1] and nums[i] not in visited:
+                temp =lower(nums[i]) 
+                visited.remove(ans[temp])
+                ans[temp] = nums[i]
+            visited.add(nums[i])
+        return len(ans)
+
+
+        
